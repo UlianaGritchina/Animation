71e7ebbebb91d8ec16304dad1cd00888f2c29e5f
UlianaGritchina <ugritchina@icloud.com>
2022-01-26 14:52:57 +0300

    commit

---
+++ Animation/Models/Animation.swift
@@ -0,0 +1,15 @@
+//
+//  Animation.swift
+//  Animation
+//
+//  Created by Ульяна Гритчина on 26.01.2022.
+//
+
+import Foundation
+
+struct Animation {
+    var name: String
+    var force: Float
+    var delay: Float
+    var dutation: Float
+}
--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -5,15 +5,58 @@
 //  Created by Ульяна Гритчина on 26.01.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var animationView: SpringView!
+    
+    @IBOutlet var animationButton: UIButton!
+    
+    @IBOutlet var animationName: UILabel!
+    @IBOutlet var forceLable: UILabel!
+    @IBOutlet var durationLable: UILabel!
+    @IBOutlet var delayLable: UILabel!
+    
+    private var animations = ["pop", "shake", "morph", "squeeze", "wobble"]
+    private var animation = Animation(
+        name: "pop",
+        force: Float.random(in: 1...3),
+        delay:  Float.random(in: 0...1),
+        dutation:  Float.random(in: 1...3)
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        animationView.layer.cornerRadius = 10
+        animationButton.layer.cornerRadius = 10
+        
+        animationName.text = animation.name
+        forceLable.text = String(format: "%.2f", animation.force)
+        durationLable.text = String(format: "%.2f", animation.dutation)
+        delayLable.text = String(format: "%.2f", animation.delay)
     }
 
-
+    @IBAction func start(_ sender: UIButton) {
+        
+        animationName.text = animation.name
+        forceLable.text = String(format: "%.2f", animation.force)
+        durationLable.text = String(format: "%.2f", animation.dutation)
+        delayLable.text = String(format: "%.2f", animation.delay)
+        
+        animationView.animation = animation.name
+        animationView.delay = CGFloat(animation.delay)
+        animationView.duration = CGFloat(animation.dutation)
+        
+        animationView.animate()
+        
+        animation.name = animations.randomElement() ?? "pop"
+        animation.force = Float.random(in: 1...3)
+        animation.delay = Float.random(in: 0...1)
+        animation.dutation = Float.random(in: 1...3)
+        
+        sender.setTitle(animation.name, for: .normal)
+    }
 }
 
